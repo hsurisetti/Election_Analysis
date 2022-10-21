2830154577addf0f779243c7e904d0531c9280c6
hsurisetti <himahere@gmail.com>
2022-10-21 11:19:40 -0400

    Test file

---
+++ Phython_practice.py
@@ -0,0 +1,38 @@
+
+print("Hello World")
+'''
+print(type(73.81))
+print(type(""))
+
+print(5 + 2 * 3)
+print(8 // 5 - 3)
+print(8 + 22 * 2 - 4)
+print(16 - 3 / 2 + 7 - 1)
+print(3 ** 3 % 5)
+print(5 + 9 * 3 / 2 - 4)
+'''
+
+my_list = []
+print(my_list)
+
+my_nlist = list()
+print(my_nlist)
+
+counties = ["Arapahoe", "Denver","Jefferson"]
+print(counties)
+
+counties.append('El Paso')
+print(counties)
+
+counties.remove('Arapahoe')
+print(counties)
+
+#counties.sort(reverse=True)
+
+print(counties)
+my_tuple = tuple()
+
+counties_tuple = ("A","D","J")
+print(counties_tuple)
+print(counties_tuple[:2])
+
